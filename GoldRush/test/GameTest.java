f43b6f504f80a57b4f43e62eeea4ef5f48b87769
Dustin Iser <diser@asphaltpanthers.com>
2019-06-05 15:16:02 -0400

    Gold rush project structure.

---
+++ GoldRush/test/GameTest.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class GameTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
